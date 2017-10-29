537abc21824b5ce92cc64198d7f60375600a2ae9
Maria Thomas <malethr@gmail.com>
2017-10-28 20:47:50 -0700

    uncomment the shared preferences to retrieve data

--- app/src/main/java/com/epicodus/dentistfinder/ui/DentistListActivity.java
+++ app/src/main/java/com/epicodus/dentistfinder/ui/DentistListActivity.java
@@ -3,11 +3,16 @@ package com.epicodus.dentistfinder.ui;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.epicodus.dentistfinder.Constants;
 import com.epicodus.dentistfinder.R;
@@ -25,12 +30,12 @@ import okhttp3.Callback;
 import okhttp3.Response;
 
 public class DentistListActivity extends AppCompatActivity {
-//    private SharedPreferences mSharedPreferences;
-//    private String mRecentAddress;
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+    private String mRecentAddress;
 
     @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
     private DentistListAdapter mAdapter;
-
     public ArrayList<Dentist> dentists = new ArrayList<>();
 
     @Override
@@ -45,17 +50,53 @@ public class DentistListActivity extends AppCompatActivity {
 
         getDentists(inputSearch);
 
-//        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-//        mRecentAddress = mSharedPreferences.getString(Constants.PREFERENCES_LOCATION_KEY, null);
-//        if (mRecentAddress != null) {
-//            getDentists(mRecentAddress);
-//        }
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mRecentAddress = mSharedPreferences.getString(Constants.PREFERENCES_LOCATION_KEY, null);
+        if (mRecentAddress != null) {
+            getDentists(mRecentAddress);
+        }
     }
 
-    private void getDentists(String location) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+        ButterKnife.bind(this);
+
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mSharedPreferences.edit();
+
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                addToSharedPreferences(query);
+                getDentists(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+
+        });
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void getDentists(String inputSearch) {
         final BetterDoctorService betterDoctorService = new BetterDoctorService();
 
-        betterDoctorService.findDentists(location, new Callback() {
+        betterDoctorService.findDentists(inputSearch, new Callback() {
 
             @Override
             public void onFailure(Call call, IOException e) {
@@ -79,5 +120,9 @@ public class DentistListActivity extends AppCompatActivity {
                 });
             }
         });
+    }
+
+    private void addToSharedPreferences(String location) {
+        mEditor.putString(Constants.PREFERENCES_LOCATION_KEY, location).apply();
     }
 }
